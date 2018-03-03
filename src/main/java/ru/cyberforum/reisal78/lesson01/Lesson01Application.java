4dc2556630bc5524f3c4999c9b617163cbc6f49e
Svetlana <xodik@mail.ru>
2018-03-03 19:09:20 +0300

    end

--- src/main/java/ru/cyberforum/reisal78/lesson01/Lesson01Application.java
+++ src/main/java/ru/cyberforum/reisal78/lesson01/Lesson01Application.java
@@ -8,12 +8,37 @@ public class Lesson01Application implements Lesson01Data {
 
     public static void main(String[] args) {
         Lesson01 lesson01 = new Lesson01JFrame();
-        lesson01.setSize(800, 600);
+        int widthWindows = 1920;
+        int heightWindows = 1080;
+        int distance = 60;
+
+        lesson01.setSize(widthWindows, heightWindows);
         lesson01.addShare(share1);
         lesson01.addShare(share2);
         lesson01.addShare(share3);
         lesson01.addShare(share4);
+        share2.width = 80;
 
+
+        Help help = new Help();
+        share2.width = share2.width * Help.messageLast(123);
+
+
+//теперь на центр
+
+        share4.x = widthWindows / 2 - share4.width / 2;
+        share4.y = heightWindows / 2 - (share1.height + distance) / 2;
+
+
+        share1.x = widthWindows / 2 - share2.width / 2 - share1.width;
+        share1.y = share4.y + distance;
+
+
+        share2.x = widthWindows / 2 - share2.width / 2;
+        share2.y = share1.y;
+
+        share3.x = widthWindows / 2 + share2.width / 2;
+        share3.y = share2.y;
 
 
     }
